e06bce209e914fb4f49599b64baf6d0a20fc7558
Ervin Rodriguez <ervin@agency-technology.com>
2020-10-29 13:32:28 -0500

    Played with React 1

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1>Hello World</h1>
       <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+export function App2() {
+  return (
+    <div className="App2">
+      <p>Testing function calling.</p>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import App from "./App";
+import App, { App2 } from "./App";
 
+/**ReactDOM.render(WHAT YOU WANT TO SHOW, WHERE TO SHOW IT) */
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
